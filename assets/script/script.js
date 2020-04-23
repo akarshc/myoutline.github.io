4717af3015e85fa4018b45873540e6eefe82fa9e
akarshc <akarshc1997@gmail.com>
2020-04-23 13:04:00 +0530

    Adding vieww

--- assets/script/script.js
+++ assets/script/script.js
@@ -14,14 +14,14 @@ window.addEventListener('click', (e) => {
 
 var openMenu = () => {
 	document.getElementById('menu').style.animation = "popIn .20s ease-in-out forwards";
-	document.getElementById('menu_icon_child_1').style.animation = "rotateAdd_1 .3s ease-in-out forwards";
-	document.getElementById('menu_icon_child_2').style.animation = "rotateAdd_2 .3s ease-in-out forwards";
+	document.getElementById('menu_icon_child_1').style.animation = "rotateAdd_1 .2s ease-in-out forwards";
+	document.getElementById('menu_icon_child_2').style.animation = "rotateAdd_2 .2s ease-in-out forwards";
 	setTimeout(function(){ open_s = 1; }, 100);
 }
 
 var closeMenu = (e) => {
 	document.getElementById('menu').style.animation = "popOut .20s ease-in-out forwards";
-	document.getElementById('menu_icon_child_1').style.animation = "rotateRemove_1 .3s ease-in-out forwards";
-	document.getElementById('menu_icon_child_2').style.animation = "rotateRemove_2 .3s ease-in-out forwards";
+	document.getElementById('menu_icon_child_1').style.animation = "rotateRemove_1 .2s ease-in-out forwards";
+	document.getElementById('menu_icon_child_2').style.animation = "rotateRemove_2 .2s ease-in-out forwards";
 	setTimeout(function(){ open_s = 0; }, 100);
 }
