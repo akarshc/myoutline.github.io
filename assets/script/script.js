be68bebdd0dad459b0eeed45514de84d04ab9cb1
fahad-kp <fahadkp999@gmail.com>
2020-10-02 23:26:24 +0530

    index page Dark mode
    Dark mode added only in index page

--- assets/script/script.js
+++ assets/script/script.js
@@ -25,3 +25,24 @@ var closeMenu = (e) => {
 	document.getElementById('menu_icon_child_2').style.animation = "rotateRemove_2 .2s ease-in-out forwards";
 	setTimeout(function(){ open_s = 0; }, 100);
 }
+
+function darkMode(){
+	currentvalue = document.getElementById('onoff').value;
+	if(currentvalue == "dark"){
+		var element = document.getElementById("container");
+		element.classList.add("dark");
+		document.getElementById("onoff").value="light";   
+		document.getElementById("onoff").innerHTML="Light Mode";
+		document.getElementById("menu_icon_child_1").classList.add("dark-menu-ico");
+		document.getElementById("menu_icon_child_2").classList.add("dark-menu-ico");
+		document.getElementById("menu_icon").classList.add("dark-menu-dark");		
+	}else{
+		document.getElementById("menu_icon_child_1").classList.remove("dark-menu-ico");
+		document.getElementById("menu_icon_child_2").classList.remove("dark-menu-ico");
+		var element = document.getElementById("container");
+		element.classList.remove("dark");
+		document.getElementById("onoff").value="dark"; 
+		document.getElementById("onoff").innerHTML="Dark Mode";
+	}
+		
+}
