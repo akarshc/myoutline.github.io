eaef881d9fc4e4b9dcd7efa8f7509cf6ecb71c9b
akarshc <akarshc1997@gmail.com>
2020-11-06 15:00:04 +0530

    Changing some UI

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,27 +1,27 @@
 
-var open_s = 0;
-
-window.addEventListener('click', (e) => {   
-	if(open_s) {
-		if (document.getElementById('menu').contains(e.target)){
-			// event triggered
-		}
-		else {
-			closeMenu();
-		}
-	}
-});
-
-var openMenu = () => {
-	document.getElementById('menu').style.animation = "popIn .20s ease-in-out forwards";
-	document.getElementById('menu_icon_child_1').style.animation = "rotateAdd_1 .2s ease-in-out forwards";
-	document.getElementById('menu_icon_child_2').style.animation = "rotateAdd_2 .2s ease-in-out forwards";
-	setTimeout(function(){ open_s = 1; }, 100);
-}
-
-var closeMenu = (e) => {
-	document.getElementById('menu').style.animation = "popOut .20s ease-in-out forwards";
-	document.getElementById('menu_icon_child_1').style.animation = "rotateRemove_1 .2s ease-in-out forwards";
-	document.getElementById('menu_icon_child_2').style.animation = "rotateRemove_2 .2s ease-in-out forwards";
-	setTimeout(function(){ open_s = 0; }, 100);
-}
+// var open_s = 0;
+//
+// window.addEventListener('click', (e) => {
+// 	if(open_s) {
+// 		if (document.getElementById('menu').contains(e.target)){
+// 			// event triggered
+// 		}
+// 		else {
+// 			closeMenu();
+// 		}
+// 	}
+// });
+//
+// var openMenu = () => {
+// 	document.getElementById('menu').style.animation = "popIn .20s ease-in-out forwards";
+// 	document.getElementById('menu_icon_child_1').style.animation = "rotateAdd_1 .2s ease-in-out forwards";
+// 	document.getElementById('menu_icon_child_2').style.animation = "rotateAdd_2 .2s ease-in-out forwards";
+// 	setTimeout(function(){ open_s = 1; }, 100);
+// }
+//
+// var closeMenu = (e) => {
+// 	document.getElementById('menu').style.animation = "popOut .20s ease-in-out forwards";
+// 	document.getElementById('menu_icon_child_1').style.animation = "rotateRemove_1 .2s ease-in-out forwards";
+// 	document.getElementById('menu_icon_child_2').style.animation = "rotateRemove_2 .2s ease-in-out forwards";
+// 	setTimeout(function(){ open_s = 0; }, 100);
+// }
